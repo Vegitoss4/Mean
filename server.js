407259fc65e0c9223144af6f738609cfe637d195
Vegitoss4 <sagar.tanivade@gmail.com>
2019-01-20 22:02:02 +0530

    Node serverjs

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log('Hello');
+const http = require('http');
+const app = require('./backend/app');
+
+const normalizePort = val =>{
+    var port = parseInt(val, 10);
+
+    if(isNaN(port)){
+        //named pipe
+        return val;
+
+    }
+    if(port >= 0) {
+        //port number
+        return port;
+    }
+    return false;
+};
+const onError = error => {
+    if(error.syscall !== "listen"){
+        throw error;
+
+    }
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    switch (error.code){
+        case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+    }
+};
+
