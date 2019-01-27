07d537d4e467214f50f22e4503f27c0096279661
Vegitoss4 <sagar.tanivade@gmail.com>
2019-01-27 20:20:33 +0530

    Server Commit

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,24 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts',(req, res, next) => {
+    const posts = [
+        {
+            id: "fadf12421l",
+            title: "First server-side post",
+            content: "This is coming from the server"
+        },
+        {
+            id: "fksajflaj132",
+            title: "Second server-side post",
+            content: "This is coming from the server"
+        }
+
+    ];
+    res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HeaderComponent } from './header/header.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
